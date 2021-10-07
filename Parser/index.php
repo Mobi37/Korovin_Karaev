c60b53f082c22d17b1d9398c56e94cbdb8fe91f0
Mobi37 <61323278+Mobi37@users.noreply.github.com>
2021-10-07 15:34:57 +0500

    Update index.php

--- Parser/index.php
+++ Parser/index.php
@@ -19,7 +19,6 @@
 
     urlGet($page1 = curlGetPage('https://drugchoice.vrachirf.ru/almagel-r'));
     urlGet($page1 = curlGetPage('https://drugchoice.vrachirf.ru/bronho-munal-r'));
-    urlGet($page1 = curlGetPage('https://drugchoice.vrachirf.ru/konkor-r'));
 
     function urlGet($urlMed){
         $html = str_get_html($urlMed);
@@ -27,7 +26,8 @@
             $a = $post->find('.b-preparation-name', 0);
             $div = $post->find('.b-preparation-resume__count', 0);
             echo $a . " - ";
-            echo $div . ";WOW; ";
+            echo $div . " : ";
              
         }
     }
+    echo "EZ";
